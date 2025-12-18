ad9d325755458ea5eb09d12d26d5ebed73d4947f
256bitguy <ashishrajsri2233@gmail.com>
2025-12-18 16:49:48 +0530

    serveer

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ wss.on("connection", (ws) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on ws://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
